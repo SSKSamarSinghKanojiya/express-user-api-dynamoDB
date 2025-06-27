2466a32bd02ea717389465aabfbd0aca21b40cee
SamarSingh <samarsingh@bitcot.com>
2025-06-27 12:09:06 +0530

    yml changes

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -1,3 +1,32 @@
+// const slsw = require('serverless-webpack');
+// const nodeExternals = require('webpack-node-externals');
+
+// module.exports = {
+//   entry: slsw.lib.entries,
+//   target: 'node',
+//   // Don’t bundle node_modules except allowlisted ones
+//   externals: [nodeExternals({ allowlist: ['serverless-http'] })],
+//   mode: slsw.lib.webpack.isLocal ? 'development' : 'production',
+//   module: {
+//     rules: [
+//       {
+//         test: /\.js$/,
+//         use: 'babel-loader',
+//         exclude: /node_modules/,
+//       },
+//     ],
+//   },
+//   resolve: {
+//     extensions: ['.js', '.json'],
+//   },
+//   optimization: {
+//     // Optional: keeps handler names readable
+//     minimize: false,
+//   },
+// };
+
+
+
 const slsw = require('serverless-webpack');
 const nodeExternals = require('webpack-node-externals');
 
